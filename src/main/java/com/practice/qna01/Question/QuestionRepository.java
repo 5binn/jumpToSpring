851c6330c455b91eb12c0c906ff2ba10028586c0
본ì5binn <dhxnejqls13@naver.com>
2024-01-03 18:46:03 +0900

    qna9

--- src/main/java/com/practice/qna01/Question/QuestionRepository.java
+++ src/main/java/com/practice/qna01/Question/QuestionRepository.java
@@ -1,8 +1,11 @@
 package com.practice.qna01.Question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Page<Question> findAll(Pageable pageable);
 }
